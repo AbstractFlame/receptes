dc3a970d8b53d066ff476332d151249d796a9daf
AbstractFlame <sami.gabor@gmail.com>
2016-01-09 05:33:33 +0100

    Can modify recipe at detailWindow

--- ReceptMenedzser/ReceptMenedzser/FoodDetailsWindow.xaml.cs
+++ ReceptMenedzser/ReceptMenedzser/FoodDetailsWindow.xaml.cs
@@ -29,8 +29,10 @@ namespace ReceptMenedzser
 
             currentRecipeIndex = MainWindow.selectedRecipeIndex;
             underModification = false;
+            btn_cancelModificationRecipe.Visibility = Visibility.Hidden;
 
             UpdateRecipeData(currentRecipeIndex);
+            UpdateEnabled();
             FillLabels();
         }
 
@@ -42,9 +44,6 @@ namespace ReceptMenedzser
             DataRow receptDataRow = dataSet.Tables[0].Rows[0];
 
             textBlock_FoodFUllName.Text = receptDataRow["name"].ToString();
-            textBox_Group.Text = LanguageManager.TranslateGroup(receptDataRow["group_id"].ToString());
-            textBox_SubGroup.Text = LanguageManager.TranslateSubGroup(receptDataRow["subgroup_id"].ToString());
-            textBox_MainIngredient.Text = LanguageManager.TranslateIngredient(receptDataRow["fo_osszetevo_id"].ToString());
             textBox_Ingredients.Text = receptDataRow["acc"].ToString();
             textBox_Completion.Text = receptDataRow["desc"].ToString();
             textBox_Remark.Text = receptDataRow["com"].ToString();
@@ -52,6 +51,25 @@ namespace ReceptMenedzser
             textBox_Picture.Text = receptDataRow["foodpic"].ToString();
             textBox_Language.Text = receptDataRow["lang"].ToString();
             image_FoodImageInDetailsWindow.Source = new BitmapImage(new Uri(System.AppDomain.CurrentDomain.BaseDirectory + "images\\Food_pictures\\" + receptDataRow["foodpic"].ToString()));
+
+            string groupId = receptDataRow["group_id"].ToString();
+            string subGroupId = receptDataRow["subgroup_id"].ToString();
+            string mainIngredientId = receptDataRow["fo_osszetevo_id"].ToString();
+            FillComboBoxes(groupId, subGroupId, mainIngredientId);
+        }
+
+        private void UpdateEnabled()
+        {
+            textBlock_FoodFUllName.IsEnabled = underModification;
+            comboB_GroupSelect.IsEnabled = underModification;
+            comboB_SubGroupSelect.IsEnabled = underModification;
+            comboB_MainIngredientSelect.IsEnabled = underModification;
+            textBox_Ingredients.IsEnabled = underModification;
+            textBox_Completion.IsEnabled = underModification;
+            textBox_Remark.IsEnabled = underModification;
+            textBox_Preparation.IsEnabled = underModification;
+            textBox_Picture.IsEnabled = underModification;
+            textBox_Language.IsEnabled = underModification;
         }
 
         private void FillLabels()
@@ -82,8 +100,68 @@ namespace ReceptMenedzser
         private void btn_modifyRecipe_Click(object sender, RoutedEventArgs e)
         {
             underModification = !underModification;
-           // if (underModification)
+            UpdateEnabled();
+            if (underModification)
+            {
+                btn_modifyRecipe.Content = "Mentés";
+                btn_cancelModificationRecipe.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                btn_modifyRecipe.Content = LanguageManager.TranslateFromDictionary("125");
+                string sql = "UPDATE recept SET ";
+                sql += "name='" + textBlock_FoodFUllName.Text + "',";
+                sql += "lang='" + textBox_Language.Text + "',";
+                sql += "acc='" + textBox_Ingredients.Text + "',";
+                sql += "group_id='" + comboB_GroupSelect.SelectedValue + "',";
+                sql += "subgroup_id='" + comboB_SubGroupSelect.SelectedValue + "',";
+                sql += "fo_osszetevo_id='" + comboB_MainIngredientSelect.SelectedValue + "',";
+                sql += "prep='" + textBox_Preparation.Text + "',";
+                sql += "desc='" + textBox_Completion.Text + "',";
+                sql += "com='" + textBox_Remark.Text + "',";
+                sql += "foodpic='" + textBox_Picture.Text + "'";
+                sql += " WHERE rid = '" + MainWindow.recipeIds[currentRecipeIndex] + "'";
+                MessageBox.Show(sql);
+                DBManager.QueryCommand(sql);
+            }
+        }
 
+        private int getComboBoxIndexById(ComboBox comboBox, string Id)
+        {
+            for(int i = 0; i < comboBox.Items.Count; ++i)
+            {
+                var v = comboBox.Items.GetItemAt(i);
+                DataRowView dv = (DataRowView)v;
+                string value = dv[0].ToString();
+                if (value == Id)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        private void FillComboBoxes(string groupId, string subGroupId, string mainIngredientId)
+        {
+            // Group select
+            DataSet dataSet = DBManager.QueryDataSet("SELECT * FROM T_Group");
+            comboB_GroupSelect.ItemsSource = dataSet.Tables[0].DefaultView;
+            comboB_GroupSelect.DisplayMemberPath = dataSet.Tables[0].Columns["Desc"].ToString();
+            comboB_GroupSelect.SelectedValuePath = dataSet.Tables[0].Columns["CS_ID"].ToString();
+            comboB_GroupSelect.SelectedIndex = getComboBoxIndexById(comboB_GroupSelect, groupId);
+
+            // Subgroup select
+            dataSet = DBManager.QueryDataSet("SELECT * FROM T_Subgroup");
+            comboB_SubGroupSelect.ItemsSource = dataSet.Tables[0].DefaultView;
+            comboB_SubGroupSelect.DisplayMemberPath = dataSet.Tables[0].Columns["Desc"].ToString();
+            comboB_SubGroupSelect.SelectedValuePath = dataSet.Tables[0].Columns["SCS_ID"].ToString();
+            comboB_SubGroupSelect.SelectedIndex = getComboBoxIndexById(comboB_SubGroupSelect, subGroupId);
+
+            // Subgroup select
+            dataSet = DBManager.QueryDataSet("SELECT * FROM T_Ingredient");
+            comboB_MainIngredientSelect.ItemsSource = dataSet.Tables[0].DefaultView;
+            comboB_MainIngredientSelect.DisplayMemberPath = dataSet.Tables[0].Columns["Desc"].ToString();
+            comboB_MainIngredientSelect.SelectedValuePath = dataSet.Tables[0].Columns["SCS_ID"].ToString();
+            comboB_MainIngredientSelect.SelectedIndex = getComboBoxIndexById(comboB_MainIngredientSelect, mainIngredientId);
         }
 
         private void btn_previousRecipe_Click(object sender, RoutedEventArgs e)
@@ -140,6 +218,14 @@ namespace ReceptMenedzser
             }
 
             UpdateRecipeData(currentRecipeIndex = MainWindow.recipesLength - 1);
+        }
+
+        private void btn_cancelModificationRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            underModification = false;
+            btn_cancelModificationRecipe.Visibility = Visibility.Hidden;
+            UpdateRecipeData(currentRecipeIndex);
+            UpdateEnabled();
         }
     }
 }
